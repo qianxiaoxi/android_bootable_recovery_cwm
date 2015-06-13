cd23d82370b33035f2ca0a261201a9c5ec366be4
qianxiaoxi <747987805@qq.com>
2015-06-13 18:08:33 +0800

    en->zh

--- voldclient/Android.mk
+++ voldclient/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvoldclient
-LOCAL_SRC_FILES := commands.c dispatcher.c event_loop.c
+LOCAL_SRC_FILES := \
+    commands$(src_suffix).c \
+    dispatcher.c \
+    event_loop$(src_suffix).c \
+
 LOCAL_CFLAGS := -DMINIVOLD -Werror
 LOCAL_C_INCLUDES :=         	\
     $(LOCAL_PATH)/..       	\
@@ -10,5 +14,9 @@ LOCAL_C_INCLUDES :=         	\
     system/core/include     	\
     system/core/libcutils   	\
     system/vold
+ifeq ($(findstring fontcn,$(BOARD_USE_CUSTOM_RECOVERY_FONT)),fontcn)
+  LOCAL_CFLAGS += -DUSE_CHINESE_FONT
+  src_suffix := _cn
+endif
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
